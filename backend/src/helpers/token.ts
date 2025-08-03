7b60f1f35ce664d211d072cf181473657e95e65e
Cameron Thomas <CamThomas47@gmail.com>
2025-08-03 19:35:29 -0400

    token helper, auth route, and user model created

--- backend/src/helpers/token.ts
+++ backend/src/helpers/token.ts
@@ -4,14 +4,14 @@ import { SECRET_KEY } from "../../config";
 interface Payload {
     id: number;
     username: string;
-    isAdmin: boolean;
+    role: string;
 }
 
-const createToken = ({id, username, isAdmin}: Payload) => {
+const createToken = ({id, username, role}: Payload): {} => {
     let payload = {
         'id': id,
         "username": username,
-        "isAdmin": isAdmin || false,
+        "role": role || "member",
     };
     return jwt.sign(payload, SECRET_KEY);
 }
